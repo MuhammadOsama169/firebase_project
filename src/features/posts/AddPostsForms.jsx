4ef46c6bcb6d375c24392198524ec4c49ee89d6c
Muhammad Osama <m.osama169@gmail.com>
2023-02-07 21:11:22 +0300

    feat: fetch users and data from api

--- src/features/posts/AddPostsForms.jsx
+++ src/features/posts/AddPostsForms.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { useDispatch,useSelector } from 'react-redux';
 
-import { postAdded } from './postSlice';
+import { addNewPost } from './postSlice';
 import { selectAllUsers } from '../users/usersSlice';
 
 import React from 'react';
@@ -11,6 +11,7 @@ const AddPostsForms = () => {
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
   const [userId, setUserId] = useState('');
+  const [addRequestStatus, setAddRequestStatus] = useState('idle');
 
   const users =useSelector(selectAllUsers);
 
@@ -18,17 +19,24 @@ const AddPostsForms = () => {
   const onContentChanged = (e) => setContent(e.target.value);
   const onAuthorChanged = (e) => setUserId(e.target.value);
 
+  const canSave = [title,content,userId].every(Boolean) && addRequestStatus ==='idle';
+  
   const onPostSave = () => {
-    if (title && content) {
-      dispatch(
-        postAdded(title, content,userId)
-      );
-      setTitle('');
-      setContent('');
-    }
-  };
+    if (canSave) {
+      try {
+          setAddRequestStatus('pending')
+          dispatch(addNewPost({ title, body: content, userId })).unwrap()
 
-  const canSave = Boolean(title) && Boolean(content) && Boolean(userId)
+          setTitle('')
+          setContent('')
+          setUserId('')
+      } catch (err) {
+          console.error('Failed to save the post', err)
+      } finally {
+          setAddRequestStatus('idle')
+      }
+  }
+  };
 
   const usersOptions = users.map(user => (
     <option key={user.id} value={user.id}>
--- src/features/posts/PostsList.jsx
+++ src/features/posts/PostsList.jsx
@@ -1,32 +1,37 @@
-import { useSelector } from 'react-redux';
-import { selectAllPosts } from './postSlice';
-import PostAuthor from './PostAuthors';
-import TimeAgo from './TimeAgo';
-import ReactionButtons from './ReactionButtons';
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { selectAllPosts ,getPostError,getPostStatus, fetchPosts } from './postSlice';
+import PostsExcerpt from './PostsExcerpt';
 
 import React from 'react';
 
 const PostsList = () => {
+  const dispatch =useDispatch();
   const posts = useSelector(selectAllPosts);
-  // Reorders and sorts posts based on time. So newest posts goes on top
-  const orderPosts=posts.slice().sort((a,b) => b.date.localeCompare(a.date))
+  const postStatus = useSelector(getPostStatus);
+  const error = useSelector(getPostError);
 
-  const renderedPosts = orderPosts.map((post) => (
-    <article key={post.id}>
-      <h3>{post.title}</h3>
-      <p>{post.content.substring(0, 100)}</p>
-      <p className="postCredit">
-        <PostAuthor userId={post.userId} />
-        <TimeAgo timestamp={post.date} />
-      </p>
-      <ReactionButtons post={post} />
-    </article>
-  ));
+  useEffect(() => {
+    if (postStatus === 'idle') {
+        dispatch(fetchPosts())
+    }
+}, [postStatus, dispatch])
+
+  let content;
+  if (postStatus === 'loading') {
+      content = <p>"Loading..."</p>;
+  } else if (postStatus === 'succeeded') {
+      // Reorders and sorts posts based on time. So newest posts goes on top
+      const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
+      content = orderedPosts.map(post => <PostsExcerpt key={post.id} post={post} />)
+  } else if (postStatus === 'failed') {
+      content = <p>{error}</p>;
+  }
 
   return (
     <section>
       <h2>Posts</h2>
-      {renderedPosts}
+      {content}
     </section>
   );
 };
