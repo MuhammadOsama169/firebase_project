8805b8a14341ba09dde1284d57ed3b5c23fe7c9e
Muhammad Osama <m.osama169@gmail.com>
2023-02-08 23:32:15 +0300

    feat: add update posts feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
-import { useState } from 'react'
 import PostsList from './features/posts/PostsList'
 import AddPostsForms from './features/posts/AddPostsForms'
+import SinglePostPage from './features/posts/SinglePostPage'
+import EditPostForm from './features/posts/EditPostForm'
+import Layout from './components/Layout'
+import { Routes,Route } from 'react-router-dom'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <main className="App">
-      <AddPostsForms/>
-      <PostsList/>
-    </main>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+
+        <Route index element={<PostsList />} />
+
+        <Route path="post">
+          <Route index element={<AddPostsForms />} />
+          <Route path=":postId" element={<SinglePostPage />} />
+          <Route path="edit/:postId" element={<EditPostForm />} />
+        </Route>
+
+      </Route>
+    </Routes>
   )
 }
 
